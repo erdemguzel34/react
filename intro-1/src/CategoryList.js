419cc6b2beceebdbca4cc079eb46deb34453f681
erdemguzel34 <erdemguzelmac34@icloud.com>
2020-11-02 21:01:55 +0300

    Event kullanımı
    onClick event'ini koda dahil ettik.
    React'ta hemen her zaman arrow functions kullanılır.
    Bunun sebebi this ifadesini kullandığımızda extend edilen Component'e karşılık geldiğinden eğer normal function yazarsak this'le component'e erişildiği gibi erişilemeyeceğinden dolayı arrow function'lar kullanılır.
    Normal fonksiyonlarda tek tek tanımlamak gerekiyor çünkü erişilemiyor.

--- intro-1/src/CategoryList.js
+++ intro-1/src/CategoryList.js
@@ -8,22 +8,27 @@ export default class Categorylist extends Component {
             categories: [
                 {categoryId: 1, categoryName: "Beverages"},
                 {categoryId: 2, categoryName: "Condiments"}
-            ]
+            ],
+            currentCategory: ""
         };
     }
 
-
+    changeCategory = (category) => {
+        this.setState({currentCategory:category.categoryName})
+    }
+    
     render() {
         return (
             <div>
                 <h3>{this.props.info.title}</h3>
                 <ListGroup>
                     {this.state.categories.map(category => (
-                        <ListGroupItem key = {category.categoryId}>
+                        <ListGroupItem onClick={() => this.changeCategory(category)} key = {category.categoryId}>
                             {category.categoryName}
                         </ListGroupItem>
                     ))}
                  </ListGroup>
+                 <h4>{this.state.currentCategory}</h4>
             </div>
         )
     }
