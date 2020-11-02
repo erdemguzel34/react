2558927f7c49ca29099764005580c8a83a30e4f9
erdemguzel34 <erdemguzelmac34@icloud.com>
2020-11-02 20:45:03 +0300

    State kullanımı
    State bir component'in datasıdır.
    JS'de bir değer saklamak istediğimizde değişken oluşturmak için let, var, const tanımlamalarından yararlandığımız gibi, React component'lerinde de değer saklamak için "state'leri" kullanırız.
    Örneğin, categorylist'in data'sına sadece categorylist'te ihtiyacımız olduğu için direkt onun üzerinde tutmak isteriz.
    İlgili data'yı ilgil yerde tutmazı sağlıyor kısaca.

--- intro-1/src/CategoryList.js
+++ intro-1/src/CategoryList.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import {ListGroup, ListGroupItem, Badge} from 'reactstrap';
 
 export default class Categorylist extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: [
+                {categoryId: 1, categoryName: "Beverages"},
+                {categoryId: 2, categoryName: "Condiments"}
+            ]
+        };
+    }
+
+
     render() {
         return (
             <div>
                 <h3>{this.props.info.title}</h3>
-                <h2>CategoryList component</h2>
                 <ListGroup>
-                    <ListGroupItem disabled tag="a" href="#">Cras justo odio</ListGroupItem>
-                    <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
-                    <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
-                    <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
+                    {this.state.categories.map(category => (
+                        <ListGroupItem key = {category.categoryId}>
+                            {category.categoryName}
+                        </ListGroupItem>
+                    ))}
                  </ListGroup>
             </div>
         )
